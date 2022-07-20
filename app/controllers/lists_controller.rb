21f51663b14f3b3b69edd94b4b447e8f1cec84b6
Monica Jara <jara.monica@gmail.com>
2022-07-20 22:36:12 +0200

    layout and added some links and style

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,6 +22,12 @@ class ListsController < ApplicationController
     @bookmark = Bookmark.new
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+
   private
 
   def set_list
